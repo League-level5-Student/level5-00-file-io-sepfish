1176f28f6dcc2788494234e6a4ac5d9f06d251f7
League Student <league@iMac-13.attlocal.net>
2019-11-29 18:26:37 -0800

    File Recorder

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,28 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		FileRecorder fr = new FileRecorder();
+		fr.program();
+	}
+	
+	void program() {
+		String s = JOptionPane.showInputDialog("Enter a message to save to a file:");
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/file.txt");
+			fw.write(s);
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
